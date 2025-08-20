c6304ad33f606d76370be6efdaae3a3f497992ff
Manojcc <manojc0005@gmail.com>
2025-08-20 15:03:34 +0530

    Update urls.py

--- server/config/urls.py
+++ server/config/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/blogs/', include('blog.urls')),
     path('api/auth/', include('users.urls')),
+
+    # ✅ React frontend catch-all
+    re_path(r'^.*$', TemplateView.as_view(template_name="index.html")),
 ]
